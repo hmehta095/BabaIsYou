69ed9082cbc328cb649fa3b82a736a0a2349fe57
himanshu <hmehta095@gmail.com>
2019-10-17 12:11:00 -0400

    All Hitbox and physics is done

--- BabaIsYou-F19/GameScene.swift
+++ BabaIsYou-F19/GameScene.swift
@@ -12,10 +12,109 @@ import GameplayKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
 
     var player:SKSpriteNode!
+    var stopBlock:SKSpriteNode!
+    var wallBlock:SKSpriteNode!
+    var isBlock:SKSpriteNode!
+    var winBlock:SKSpriteNode!
+    var flagBlock:SKSpriteNode!
+    var flag:SKSpriteNode!
+
     let PLAYER_SPEED:CGFloat = 20
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
+        
+        self.flag = self.childNode(withName: "flag") as! SKSpriteNode
+                
+                self.flag.physicsBody = SKPhysicsBody(rectangleOf: flag.size)
+                self.flag.physicsBody?.affectedByGravity = false
+                self.flag.physicsBody?.categoryBitMask = 1
+        //        self.flag.physicsBody?.collisionBitMask = 0
+                self.flag.physicsBody?.contactTestBitMask = 4
+        
+        
         self.player = self.childNode(withName: "baba") as! SKSpriteNode
+        
+        self.player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        self.player.physicsBody?.affectedByGravity = false
+        self.player.physicsBody?.categoryBitMask = 4
+//        self.player.physicsBody?.collisionBitMask = 0
+//        self.player.physicsBody?.contactTestBitMask = 0
+        
+        self.stopBlock = self.childNode(withName: "stopblock") as! SKSpriteNode
+        
+        self.stopBlock.physicsBody = SKPhysicsBody(rectangleOf: stopBlock.size)
+        self.stopBlock.physicsBody?.affectedByGravity = false
+        self.stopBlock.physicsBody?.categoryBitMask = 8
+//        self.stopBlock.physicsBody?.collisionBitMask = 0
+//        self.stopBlock.physicsBody?.contactTestBitMask = 0
+        
+        
+         self.wallBlock = self.childNode(withName: "wallblock") as! SKSpriteNode
+                
+                self.wallBlock.physicsBody = SKPhysicsBody(rectangleOf: wallBlock.size)
+                self.wallBlock.physicsBody?.affectedByGravity = false
+                self.wallBlock.physicsBody?.categoryBitMask = 16
+        //        self.wallBlock.physicsBody?.collisionBitMask = 0
+        //        self.wallBlock.physicsBody?.contactTestBitMask = 0
+        
+        self.flagBlock = self.childNode(withName: "flagblock") as! SKSpriteNode
+                
+                self.flagBlock.physicsBody = SKPhysicsBody(rectangleOf: wallBlock.size)
+                self.flagBlock.physicsBody?.affectedByGravity = false
+                self.flagBlock.physicsBody?.categoryBitMask = 256
+        //        self.wallBlock.physicsBody?.collisionBitMask = 0
+        //        self.wallBlock.physicsBody?.contactTestBitMask = 0
+        
+        
+        
+//        self.enumerateChildNodes(withName: "isblock") {
+//            (node, stop) in
+//            let isBlock = node as! SKSpriteNode
+//            isBlock.physicsBody = SKPhysicsBody(rectangleOf: isBlock.size)
+////            stick.physicsBody?.affectedByGravity = true
+////            stick.physicsBody?.categoryBitMask = 16
+//
+//            isBlock.physicsBody?.affectedByGravity = false
+//                           isBlock.physicsBody?.categoryBitMask = 32
+//                           isBlock.physicsBody?.collisionBitMask = 0
+//                           isBlock.physicsBody?.contactTestBitMask = 0
+//        }
+        
+        
+        self.isBlock = self.childNode(withName: "isblock") as! SKSpriteNode
+                       
+                       self.isBlock.physicsBody = SKPhysicsBody(rectangleOf: isBlock.size)
+                       self.isBlock.physicsBody?.affectedByGravity = false
+                       self.isBlock.physicsBody?.categoryBitMask = 32
+                       self.isBlock.physicsBody?.collisionBitMask = 0
+                       self.isBlock.physicsBody?.contactTestBitMask = 0
+        self.isBlock = self.childNode(withName: "iblock") as! SKSpriteNode
+        
+        self.isBlock.physicsBody = SKPhysicsBody(rectangleOf: isBlock.size)
+        self.isBlock.physicsBody?.affectedByGravity = false
+        self.isBlock.physicsBody?.categoryBitMask = 32
+        self.isBlock.physicsBody?.collisionBitMask = 0
+        self.isBlock.physicsBody?.contactTestBitMask = 0
+               
+        
+        self.winBlock = self.childNode(withName: "winblock") as! SKSpriteNode
+        
+        self.winBlock.physicsBody = SKPhysicsBody(rectangleOf: isBlock.size)
+        self.winBlock.physicsBody?.affectedByGravity = false
+        self.winBlock.physicsBody?.categoryBitMask = 64
+//        self.winBlock.physicsBody?.collisionBitMask = 0
+//        self.winBlock.physicsBody?.contactTestBitMask = 0
+        
+        self.enumerateChildNodes(withName: "wall") {
+            (node, stop) in
+            let stick = node as! SKSpriteNode
+            stick.physicsBody = SKPhysicsBody(rectangleOf: stick.size)
+            stick.physicsBody?.affectedByGravity = false
+            stick.physicsBody?.categoryBitMask = 128
+            stick.physicsBody?.collisionBitMask = 0
+        }
+        
+        
     }
    
     func didBegin(_ contact: SKPhysicsContact) {
